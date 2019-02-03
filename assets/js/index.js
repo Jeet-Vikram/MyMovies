1117f2184bd643d9d91022eedffe21559c3c796e
Vikram Jeet Shyoran <vikramjeetshyoran1998@gmail.com>
2019-02-03 19:43:33 +0530

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,27 @@
+    // For navbar
+
+$(".nav-link").mouseover(function(){
+  $(this).css("color","Green");
+})
+$(".hvr").mouseout(function(){
+  $(this).css("color","rgba(255,255,255,.5)");
+})
+$(".home").mouseout(function(){
+  $(this).css("color","white");
+})
+
+$("#carousel-2").carousel({
+  interval:false
+});
+$("#carousel-3").carousel({
+  interval:false
+});
+$("#carousel-4").carousel({
+  interval:false
+});
+$("#carousel-5").carousel({
+  interval:false
+});
+$("#carousel-6").carousel({
+  interval:false
+});
